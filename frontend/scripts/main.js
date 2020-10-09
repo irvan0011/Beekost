b0555a72becee083b040feabf882752bed2eae1c
irvan0011 <69573962+irvan0011@users.noreply.github.com>
2020-10-10 02:30:10 +0700

    Add files via upload

---
+++ frontend/scripts/main.js
@@ -0,0 +1,11 @@
+$(function () {
+    $('.container-card-list').slick({
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        nextArrow: $('.next'),
+        prevArrow: $('.prev'),
+    });
+
+});
